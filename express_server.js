a128ac1a2ff34c4494cd4f6f59bac3ad0fd634ee
Ryan Olejnik <ryan.olejnik@gmail.com>
2018-04-18 00:01:52 +0000

    url input form added

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const app = require('express')();
 var PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 var urlDatabase = {
@@ -8,16 +10,32 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// HOMEPAGE
 app.get("/", (request, response) => {
   // console.log('this is the sauce', request);
   response.render('home_page.ejs');
 });
 
+// NEW URL FORM PAGE:
+app.get('/urls/new', function (request, response){
+  response.render('urls_new.ejs');
+});
+
+app.post('/urls', (request, response) => {
+  console.log('request.body =\n', request.body);
+  response.end('THANKS for the url!!');
+});
+
+
+
+// LIST OF ALL URLS
 app.get('/urls', function(request, response){
   let templateVariables = {urls: urlDatabase };
   response.render('urls_index', templateVariables);
 });
 
+
+// SINGLE URL 
 app.get('/urls/:id', function(request, response){
   let templateVariables = {'shortURL': request.params.id, 'longURL': urlDatabase[request.params.id]};
   response.render('urls_show.ejs', templateVariables);
